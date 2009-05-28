d69d2bf39842c032ea4660a10556e78b14b6d71c
David Voswinkel <david@optixx.org>
2009-05-28 17:49:50 +0200

    o add snes diskio interface

---
+++ snes/fatfstest/config.h
@@ -0,0 +1,18 @@
+
+#define CMD_INIT        0x00
+#define CMD_READ        0x01
+#define CMD_WRITE       0x02
+#define CMD_NONE        0xff
+
+/* Memory Map */
+#define MMIO_CMD        0x3010
+#define MMIO_SECTOR01   0x3011
+#define MMIO_SECTOR02   0x3012
+#define MMIO_SECTOR03   0x3013
+#define MMIO_SECTOR04   0x3014
+#define MMIO_COUNT      0x3015
+#define MMIO_RETVAL     0x3016
+
+
+#define SHARED_SIZE     512
+#define SHARED_ADDR     0x3f0000
--- snes/fatfstest/debug.h
+++ snes/fatfstest/debug.h
@@ -1,6 +1,5 @@
-void initDebugMap(void);
-void debug(void);
+void debug_init(void);
+void debug_enable(void);
 void int2hex(unsigned long i, char *buf,word size);
-void printDebugScreen(char* line,word y);
-void enableDebugScreen(void);
-void printf(char* line);
+void print_screen(char* line,word y);
+void print_console(char* line);
