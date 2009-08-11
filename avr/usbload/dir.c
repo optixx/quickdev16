cf090451df3f8cc81d2913d374dd3d4ff789a20d
optixx <david@optixx.org>
2009-08-11 15:42:14 +0200

    add dir ent to sram copy

---
+++ avr/usbload/dir.c
@@ -0,0 +1,46 @@
+/*
+ * =====================================================================================
+ *
+ * ________        .__        __    ________               ____  ________
+ * \_____  \  __ __|__| ____ |  | __\______ \   _______  _/_   |/  _____/
+ *  /  / \  \|  |  \  |/ ___\|  |/ / |    |  \_/ __ \  \/ /|   /   __  \
+ * /   \_/.  \  |  /  \  \___|    <  |    `   \  ___/\   / |   \  |__\  \
+ * \_____\ \_/____/|__|\___  >__|_ \/_______  /\___  >\_/  |___|\_____  /
+ *        \__>             \/     \/        \/     \/                 \/
+ *
+ *                                  www.optixx.org
+ *
+ *
+ *        Version:  1.0
+ *        Created:  07/21/2009 03:32:16 PM
+ *         Author:  david@optixx.org
+ *
+ * =====================================================================================
+ */
+
+
+#include "dir.h"
+
+#include <string.h>
+
+
+uint16_t positon = 0;
+
+void dir_entry_start(){
+    positon = 0;
+}
+
+void dir_add_entry(uint16_t id, uint8_t file_name,uint32_t file_size,uint8_t file_attr){
+    uint32_t addr;
+    dir_ent_t ent;
+    strncpy(ent.file_name,file_name,13);
+    ent.file_size = file_size;
+    ent.file_attr = file_attr;
+    addr = DIR_ENTRY_LOC + (positon << DIR_ENTRY_SIZE_SHIFT );
+    sram_bulk_copy(addr, (uint8_t *) &ent, DIR_ENTRY_SIZE );
+    positon++;
+
+}
+
+
+
