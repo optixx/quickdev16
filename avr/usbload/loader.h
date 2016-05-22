15abd671fed2bb576241b2d19b4285e28379b64f
optixx <david@optixx.org>
2016-05-22 15:13:49 +0200

    Build current firmware with rle compression

--- avr/usbload/loader.h
+++ avr/usbload/loader.h
@@ -1,15 +1,16 @@
 /*
-File: qd16boot01.smc
-Time: Sun, 21 Feb 2016 18:47:17
+File: qd16boot02.smc
+Time: Wed, 18 May 2016 15:44:45
 */
 #ifndef __FIFO_H__
 #define __FIFO_H__
 
-#define LOADER_NAME "qd16boot01.smc"
+#define LOADER_NAME "qd16boot02.smc"
 #define LOADER_COMPRESS "ZIP"
-#define ROM_ZIP_SIZE     17897
-#define ROM_BUFFER_CNT   1
+#define ROM_ZIP_SIZE     33654
+#define ROM_BUFFER_CNT   2
 
-#define ROM_BUFFER_SIZE01  17897
+#define ROM_BUFFER_SIZE01  32767
+#define ROM_BUFFER_SIZE02  887
 
 #endif
